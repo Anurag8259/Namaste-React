65963d3e109ca040e5c0f1d51c1a7eaad1f9b870
KIIT <KIIT@example.com>
2026-07-01 16:52:52 +0530

    Episode 3 - JSX , React Functional Component

--- App.js
+++ App.js
@@ -2,25 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement("h1", {id : "heading"} , "Hello World from React!"); // {} - attributes , eg ; {id: "title"} 
 
-// heading : If we do console.log(heading) , it will give us a object which is called React Element. It is a object representation of the HTML element.
-// props : The attributes passed in the second argument of React.createElement()
+
+const Title = () => (
+    <div>
+        {/* <Heading/>   --  It will lead to infinite loop ---  we can call a component inside an element/component*/}
+        <h1>This is a Title !! </h1>
+    </div>
+);
+
+
+const head = (
+    <h1>
+        <Title/>
+        Hello from React Element
+    </h1>
+);
+
+const number = 1000;
+
+const Heading = () => (
+    <div id = "container">
+        {head}
+        {1+10}
+        <h2>{number}</h2>
+        {/* We can use JavaScript expressions inside curly braces. */}
+        <Title/> 
+        {/* // This is called Composition of Components. We can use one component inside another component. */}
+        <h1 className="heading" tabIndex = "5">Hello from React Functional Component</h1>
+    </div>
+);
+//OR
+// We can also write this like a normal js function instead of arrow function. But we need to use return keyword in that case.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading); // Renders the React element into the DOM
+// root.render(Heading); - WRONG
 
-
-
-const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "Hello from h1 tag"))); // Object / react element
-// root.render(parent);
-
-
-
-
-const parent1 = React.createElement("div", { id: "parent2" }, React.createElement("div", { id: "child2" }, 
-    [React.createElement("h1", {}, "Hello from h1 tag"), React.createElement("h2", {}, "Hello from h2 tag")]));
-root.render(parent1); // Content of root will be replaced with parent1 and previous content will be removed. So only last render will work.
-
-// i am using multiple render in this file but why is only last render working ? - Tis is because ReactDOM.render() replaces the existing content of the root element with the new content. When you call root.render() multiple times, it will overwrite the previous render and only display the last one. If you want to render multiple elements, you should combine them into a single React element or use a parent container to hold them together.
-// Due to this nested lengthy code , we have JSX.
+root.render(<Heading/>); //Correct
